3aeb3fdae9901ce3b1280a6335177f549fc4b15e
TonyBRoman <tonybaggon@gmail.com>
2026-03-20 23:41:16 -0600

    Robust Error Handing

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities")
+const utilities = require("./utilities/")
 
 /* ***********************
  * Routes
@@ -35,7 +35,7 @@ const port = process.env.PORT
 const host = process.env.HOST
 
 // index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 /* 404 handler */
 app.use(async (req, res, next) => {
@@ -54,9 +54,15 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  let message
+  if (err.status == 404) {
+    message = err.message
+  } else {
+    message = 'Oh no, There was a crash. Maybe try a different route?'
+  }
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
