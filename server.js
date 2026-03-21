14e7c1ab571f4af7623e5163ebb12e097e6af31a
TonyBRoman <tonybaggon@gmail.com>
2026-03-20 23:24:44 -0600

    basic error handing

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities")
 
 /* ***********************
  * Routes
@@ -33,6 +34,33 @@ app.set("layout", "./layouts/layout")
 const port = process.env.PORT
 const host = process.env.HOST
 
+// index route
+app.get("/", baseController.buildHome)
+
+/* 404 handler */
+app.use(async (req, res, next) => {
+  let nav = await utilities.getNav()
+  res.status(404).render("errors/error", {
+    title: "404 - Page Not Found",
+    message: "Sorry, the page you are looking for does not exist.",
+    nav
+  })
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 /* ***********************
  * Log statement to confirm server operation
  *************************/
@@ -40,6 +68,4 @@ app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
 
-// index route
-app.get("/", baseController.buildHome)
 
