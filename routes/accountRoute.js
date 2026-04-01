2f927db574344deae605235f03aa8362c8727083
TonyBRoman <tonybaggon@gmail.com>
2026-04-01 00:05:38 -0600

    data validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,10 +2,20 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
+const regValidate = require("../utilities/account-validation")
 
+// Login view
 router.get("/login", accountController.buildLogin)
+// Register View
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 // Error handling
 router.use(function (err, req, res, next) {
